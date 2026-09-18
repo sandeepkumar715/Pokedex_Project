1f7b4364a3ffcf915fc1cbd2c6c2516795dd40f8
sandeepkumar715 <sandeeprebhnadih@gmail.com>
2026-09-18 12:46:37 +0530

    basic pokedex setup done

---
+++ src/components/PokemonList/PokemonList.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react"
+import axios from 'axios';
+import './PokemonList.css';
+import Pokemon from "../Pokemon/Pokemon";
+function PokemonList(){
+
+
+    const [pokemonList,setPokemonList]= useState([]);
+    const [isLoading,setIsLoading] =useState(true);
+    const POKEDEX_URL = 'https://pokeapi.co/api/v2/pokemon/'
+    async function downloadPokemon() {
+        //this dowloads list of 20 pokemons
+        const response = await axios.get(POKEDEX_URL);
+
+        //we get the array of pokemons from result
+        const pokemonResults = response.data.results;
+        console.log(response.data);
+
+        // iterating over the array of pokemons and using their url, to create an array of promises
+        // that will  download  those 20 pokemons 
+         const pokemonResultPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url));
+
+         //passing that promise array to aaxios.all
+         const pokemonData = await axios.all( pokemonResultPromise); // array of 20 pokemon detailed data
+         console.log(pokemonData);
+
+         //now iterate on data of each pokeemon and extract id,name,image,types
+         const pokeListResult = pokemonData.map((pokeData) => {
+              const pokemon= pokeData.data;
+              return {
+                id: pokemon.id,
+                name:pokemon.name,
+                image: (pokemon.sprites.other) ? pokemon.sprites.other.dream_world.front_default : pokemon.sprites.front_shiny,
+                types:pokemon.types
+              }
+
+         })
+         console.log( pokeListResult);
+         setPokemonList( pokeListResult);
+         
+         
+        setIsLoading(false);
+        
+    }
+    useEffect(() =>{
+        downloadPokemon();
+
+        
+    },[]) ;
+
+
+    return(
+        <div className="pokemon-list-wrapper">
+            {/* <div>
+                X: {x} <button onClick={() => setX(x+1)}>Inc</button>
+            </div>
+             <div>
+                Y: {y} <button onClick={() => setY(y+1)}>Inc</button>
+            </div> */}
+            <div>Pokemon List</div>
+
+            {(isLoading) ? 'Loading......' : 
+                 pokemonList.map((p) => <Pokemon name={p.name}  image={p.image} key={p.id}/>)}
+
+        </div>
+     )
+}
+export default PokemonList;
